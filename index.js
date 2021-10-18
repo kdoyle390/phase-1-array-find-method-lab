304ff68176157fd668fde72ff74451e22e3adf64
Kelsey <kdoyle390@gmail.com>
2021-10-18 07:38:29 -0500

    commit tree febdec4d93c687004da884ce175fcbdcf03f946d

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 
+
+
+//find() takes a callback function as an argument 
+//that returns either true or false
+
+// const record = [
+//     {year: "2018", 
+//         result: "L"},
+//     {year: "2017", 
+//         result: "W"},
+//     {year: "2016", 
+//         result: "N/A"}
+//     //...
+//   ]
+
+// function superbowlWin(x) {
+//     return x.result === 'W'
+//     }
+  
+  
+//   console.log(record.find(superbowlWin).year);
+
+//code so far returns {year: '1969', result: 'W'}
+function superbowlWin(record) {
+    if (record.result === 'W') {
+      return record.year
+      
+    }
+  }
+  
+  console.log(record.find(superbowlWin))
